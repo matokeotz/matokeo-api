1d88b2f15f2ffaffbc52b065c66ebfce98dea3d5
sdpeyton <stephendpeyton@gmail.com>
2017-09-23 16:25:58 -0400

    updated settings directory to have multiple configs for different environments

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from . import key
+
+# from key import key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gb1)heail^3w7ybep=*iclns3g-&=9kv_iehju#*$ou1bypb8i'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
