9f1578409554d1cc2e5f7e609971cf6731b4c80d
sdpeyton <stephendpeyton@gmail.com>
2017-10-08 19:03:36 -0400

    added new endpoint to view students and student grades by school

--- app/api/serializers/student_serializers.py
+++ app/api/serializers/student_serializers.py
@@ -2,10 +2,26 @@ from rest_framework.serializers import ModelSerializer, StringRelatedField, Prim
     SlugRelatedField, HyperlinkedIdentityField
 from rest_framework import serializers
 
-from api.models.student_models import Student
+from api.models.student_models import Student, StudentSubjectGrade
 
 class StudentSerializer(ModelSerializer):
 
     class Meta:
         model = Student
         fields = ('identifier', 'gender', 'aggregate_score', 'graduation_year', 'division', 'school')
+
+class StudentSubjectGradeSerializer(ModelSerializer):
+    student = StringRelatedField()
+    subject = StringRelatedField()
+    grade = StringRelatedField()
+
+    class Meta:
+        model = StudentSubjectGrade
+        fields = ('student', 'subject', 'grade')
+
+class SchoolStudentSerializer(ModelSerializer):
+    school = StringRelatedField()
+
+    class Meta:
+        model = Student
+        fields = ('school', 'graduation_year', 'gender', 'division', 'aggregate_score')
--- app/api/views/student_views.py
+++ app/api/views/student_views.py
@@ -5,9 +5,11 @@ from rest_framework.response import Response
 from django_filters.rest_framework import Filter, FilterSet
 from django_filters.fields import Lookup
 
-from api.models.student_models import Student
+from api.utils.filters import field_filter
 
-from api.serializers.student_serializers import StudentSerializer
+from api.models.student_models import Student, StudentSubjectGrade
+
+from api.serializers.student_serializers import StudentSerializer, StudentSubjectGradeSerializer, SchoolStudentSerializer
 
 class StudentView(ListAPIView):
     ''' 
@@ -17,3 +19,37 @@ class StudentView(ListAPIView):
     model = Student
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
+
+class StudentSubjectGradeFilter(FilterSet):
+    subject = Filter(name='subject__name', method=field_filter)
+    grade = Filter(name='grade__letter', method=field_filter)
+
+    class Meta:
+        model = StudentSubjectGrade
+        fields = ('student', 'subject', 'grade')
+
+class StudentSubjectGradeView(ListAPIView):
+    serializer_class = StudentSubjectGradeSerializer
+    filter_class = StudentSubjectGradeFilter
+    
+    def get_queryset(self):
+        student = self.kwargs['student']
+        return StudentSubjectGrade.objects.filter(student__identifier=student)
+
+class SchoolStudentFilter(FilterSet):
+    graduation_year = Filter(name='graduation_year', method=field_filter)
+    gender = Filter(name='gender', method=field_filter)
+    division = Filter(name='division', method=field_filter)
+    aggregate_score = Filter(name='aggregate_score', method=field_filter)
+
+    class Meta:
+        model = Student
+        fields = ('graduation_year', 'gender', 'division', 'aggregate_score')
+
+class SchoolStudentView(ListAPIView):
+    serializer_class = SchoolStudentSerializer
+    filter_class = SchoolStudentFilter
+
+    def get_queryset(self):
+        school = self.kwargs['school']
+        return Student.objects.filter(school__short_name=school)
